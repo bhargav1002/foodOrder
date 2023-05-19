9d17ec16b3802ed44dcb4b9b4f57c19d1094544c
bhargav1002 <60733610+bhargav1002@users.noreply.github.com>
2023-05-19 12:52:48 +0530

    Update Adminlogin.js

--- src/components/Adminlogin/Adminlogin.js
+++ src/components/Adminlogin/Adminlogin.js
@@ -116,7 +116,7 @@ export default function Adminlogin(props) {
             <section style={{ maxWidth: '30rem', margin: 'auto' }} className='h-screen pt-48'>
                 <div className='shadow p-4 rounded-xl bg-white' style={{ zIndex: 100 }}>
                     <div>
-                        <h2 style={{ textAlign: "center" }}>Admin Login</h2>
+                        <h2 style={{ textAlign: "center" }} className='text-3xl'>Admin Login</h2>
                     </div>
                     <form className={classes.form} onSubmit={confirmHandler}>
                         <div className={adminsigninemailControlClasses}>
